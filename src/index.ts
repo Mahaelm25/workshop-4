96fbfae824d7b5e8c4c64d1659619fd13941d06a
Mahaelm25 <maha.el_allem@edu.devinci.fr>
2025-03-27 00:20:37 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import { launchRegistry } from "./registry/registry";
 import { launchUsers } from "./users/launchUsers";
 
 export async function launchNetwork(nbNodes: number, nbUsers: number) {
-  // launch node registry
+  
   const registry = await launchRegistry();
 
-  // launch all nodes
   const onionServers = await launchOnionRouters(nbNodes);
 
-  // launch all users
   const userServers = await launchUsers(nbUsers);
 
   return [registry, ...onionServers, ...userServers];
